d8793cb5b6a745fbf380f6fa3069d1d70be0d46e
prince4772 <mameerbakhsh02@gmail.com>
2022-02-28 22:17:57 +0500

    title updtated

--- lib/main.dart
+++ lib/main.dart
@@ -26,7 +26,7 @@ class _MyAppState extends State<MyApp> {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Alfacare Health System',
+      title: 'Alfacare',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
