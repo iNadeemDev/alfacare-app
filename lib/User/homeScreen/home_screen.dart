b26346b8958ee9c376e1ffad8efeb6f88f164c5b
prince4772 <mameerbakhsh02@gmail.com>
2022-03-22 02:43:53 +0500

    navigation to City()

--- lib/User/homeScreen/home_screen.dart
+++ lib/User/homeScreen/home_screen.dart
@@ -81,7 +81,7 @@ class _HomeScreenState extends State<HomeScreen> {
                             Navigator.push(
                                 context,
                                 MaterialPageRoute(
-                                    builder: (context) => Specialities()));
+                                    builder: (context) => City()));
                           },
                           child: Container(
                               padding: EdgeInsets.fromLTRB(35, 20, 40, 35),
